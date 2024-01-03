b5d0fea2d0c2104309f008d24786270d85456c6d
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-02 19:21:05 -0500

    Admin site

--- server/countdown_app/admin.py
+++ server/countdown_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Event, Guest
 
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+  list_display = ['id', 'name', 'date']
+
+class GuestAdmin(admin.ModelAdmin):
+  list_display = ['id', 'event_id', 'name', 'confirm_code', 'assist_confirm']
+  raw_id_fields = ['event_id']
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GuestAdmin)
