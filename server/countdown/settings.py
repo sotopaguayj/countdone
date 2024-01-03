a084b02c1712d6b315b2334ed4c0a78df4c8e913
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-02 19:20:21 -0500

    Python migrations

--- server/countdown/settings.py
+++ server/countdown/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework'
+    'rest_framework',
     'countdown_app'
 ]
 
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
