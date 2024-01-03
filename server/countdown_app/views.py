15995168ae4443a3a26a630195b6f1963c29fb7f
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-02 20:30:14 -0500

    Api views

--- server/countdown_app/views.py
+++ server/countdown_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import GuestSerializer
+from .models import Guest
 
 # Create your views here.
+
+class GuestView(viewsets.ModelViewSet):
+  serializer_class = GuestSerializer
+  queryset = Guest.objects.all()
