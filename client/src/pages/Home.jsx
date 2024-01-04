450750aa741062b96dd0881807417ad4ae61de98
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-04 13:11:34 -0500

    Basic Counter Logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,37 @@
 import { useEffect, useState } from 'react'
 
 function MainPage() {
-  const [guests, setGuests] = useState([])
+  const [date, setDate] = useState([])
+  const [daysLeft, setDaysLeft] = useState(0)
+  const [monthsLeft, setMonthsLeft] = useState(0)
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/event/api/v1/guest/').then(res => res.json()).then(data => setGuests(data))
+    fetch('http://127.0.0.1:8000/event/api/v1/event').then(res => res.json()).then((data) => setDate(data[0].date))
   }, [])
+
+  useEffect(() => {
+    
+    const dateCounter = () => {
+      const eventDate = new Date(`${date}T00:00:00`).getTime()  
+      const now = new Date().getTime()
+
+      const diff = eventDate - now
+
+      const days = Math.ceil(diff / (1000 * 60 * 60 * 24))
+      const months = Math.floor(days / 30)
+      const daysLeft = months >= 1 ? days - (months * 30) : days
+
+      setDaysLeft(daysLeft)
+      setMonthsLeft(months)
+    }
+      const interval = setInterval(dateCounter, 1000)
+      return () => clearInterval(interval)
+  })
   
   return (
     <>
       <div>MainPage</div>
-      {
-        guests.map(guest => (
-          <p key={ guest.id }>{ guest.name }</p>
-        ))
-      }
+      <p>Queda aproximadamente { monthsLeft >= 1 ? `${monthsLeft} meses y` : ``} { daysLeft } días </p>
     </>
   )
 }
