e725d428e4ba1e5fddf73bf5c0f7ec077e03cd88
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-03 23:08:28 -0500

    Event Url

--- server/countdown_app/urls.py
+++ server/countdown_app/urls.py
@@ -4,7 +4,9 @@ from rest_framework.documentation import include_docs_urls
 from countdown_app import views
 
 router = routers.DefaultRouter()
-router.register(r'guest', views.GuestView, 'guest')
+router.register(r'guest', views.GuestView, 'api')
+router.register(r'event', views.EventView, 'api')
+
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('docs/', include_docs_urls(title="Event API"))
