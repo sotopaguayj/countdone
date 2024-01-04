5a2962ee47279544573092e341d69f7ab59a9758
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-03 22:01:25 -0500

    Fix: Random confirmation code

--- server/countdown_app/models.py
+++ server/countdown_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from uuid import uuid4
 from random import randint
 # Create your models here.
@@ -15,8 +17,12 @@ class Guest(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   event_id = models.ForeignKey(Event, null=True, on_delete=models.CASCADE)
   name = models.CharField(max_length=200)
-  confirm_code = models.IntegerField(default=randint(100000, 999999), editable=False)
+  confirm_code = models.IntegerField(unique=True, editable=False)
   assist_confirm = models.BooleanField(default=False)
 
   def __str__(self):
     return self.name
+
+@receiver(pre_save, sender=Guest)
+def random_number(sender, instance, **kwargs):
+  instance.confirm_code = randint(100000, 999999)
