a77768a58db8fdf941a73cfbf477e435318ae7d0
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-03 23:08:20 -0500

    Event Serializer

--- server/countdown_app/serializer.py
+++ server/countdown_app/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Guest
+from .models import Event, Guest
 
 class GuestSerializer(serializers.ModelSerializer):
   class Meta:
     model = Guest
+    fields = '__all__'
+
+class EventSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Event
     fields = '__all__'
