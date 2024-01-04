20d743efca86047a5712433b9a83897680cf2e6f
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-03 22:00:02 -0500

    Page Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Invitation from './pages/Invitation'
+import Confirm from './pages/Confirm'
 
-function App() {
 
+function App () {
   return (
-    <>
-      <h1>Event Countdown App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home /> }/>
+        <Route path='invitation/' element={ <Invitation />} />
+        <Route path='confirm_assist/' element={ <Confirm />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
