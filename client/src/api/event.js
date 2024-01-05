a71e653948c3cbe12913b2cd2c668a93fc2ecfd6
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-04 21:55:44 -0500

    Fetch Refactor

--- client/src/api/event.js
+++ client/src/api/event.js
@@ -2,10 +2,10 @@
 
 const eventDate = async() => {
   try {
-    const response = await fetch('http://127.0.0.1:8000/event/api/v1/event')
+    const response = await fetch('http://127.0.0.1:8000/event/api/v1/event/af73d455-e036-4322-931c-42463398e3d1')
     const data = await response.json()
-
-    return { isError: false, date: data[0].date }
+    
+    return { isError: false, date: data.date }
 
   } catch (error) {
 
