2c708119b09bbc357554ce3feb92667405cfc946
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-04 19:16:11 -0500

    Hooks Refactor

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,38 +1,24 @@
-import { useEffect, useState } from 'react'
+import useCountdown from '../hooks/useCountdown'
+import useDate from '../hooks/useDate'
 
-function MainPage() {
-  const [date, setDate] = useState([])
-  const [daysLeft, setDaysLeft] = useState(0)
-  const [monthsLeft, setMonthsLeft] = useState(0)
+function MainPage () {
+  const { isError, isLoading } = useDate()
+  const { daysLeft, monthsLeft } = useCountdown()
 
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/event/api/v1/event').then(res => res.json()).then((data) => setDate(data[0].date))
-  }, [])
-
-  useEffect(() => {
-    
-    const dateCounter = () => {
-      const eventDate = new Date(`${date}T00:00:00`).getTime()  
-      const now = new Date().getTime()
-
-      const diff = eventDate - now
-
-      const days = Math.ceil(diff / (1000 * 60 * 60 * 24))
-      const months = Math.floor(days / 30)
-      const daysLeft = months >= 1 ? days - (months * 30) : days
-
-      setDaysLeft(daysLeft)
-      setMonthsLeft(months)
-    }
-      const interval = setInterval(dateCounter, 1000)
-      return () => clearInterval(interval)
-  })
-  
   return (
     <>
       <div>MainPage</div>
-      <p>Queda aproximadamente { monthsLeft >= 1 ? `${monthsLeft} meses y` : ``} { daysLeft } días </p>
+      {
+        isLoading ? (
+          <p>Cargando...</p>
+        ) : !isError ? (
+          <p>Queda aproximadamente {monthsLeft >= 1 && `${ monthsLeft } meses y`} { daysLeft } días</p>
+        ) : (
+          <p>Ha ocurrido un error</p>
+        )
+      }
     </>
+
   )
 }
 
