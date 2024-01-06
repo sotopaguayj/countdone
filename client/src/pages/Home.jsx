9b90c2b12fdb32a061305fdb4871c6897f89d0d3
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-05 20:28:14 -0500

    Home Page Styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,29 +1,31 @@
 import { Link } from 'react-router-dom'
 import useCountdown from '../hooks/useCountdown'
 import useDate from '../hooks/useDate'
+
 import Digit from '../components/Digit'
 
 function MainPage() {
   const { isError, isLoading } = useDate()
   const { daysLeft, monthsLeft } = useCountdown()
+
   return (
     <>
-      <header className='text-5xl'>MainPage</header>
-      <main>
+      <header className='text-5xl uppercase text-slate-50 text-center'>MainPage</header>
+      <main className='text-center'>
         {
           isLoading ? (
             <p>Cargando...</p>
           ) : !isError ? (
-            <>
-              <Digit value={monthsLeft} />
-              <Digit value={daysLeft} />
-            </>
+            <div className='flex justify-center gap-x-4'>
+              <Digit value={ monthsLeft } name='Meses' />
+              <Digit value={ daysLeft } name='Días' />
+            </div>
           ) : (
             <p>Ha ocurrido un error</p>
           )
           }
         
-        <Link to='confirm_assist'>Confirma tu asistencia aqui!</Link>
+        <Link to='confirm_assist' className='text-white text-lg font-bold uppercase'>Confirma tu asistencia aqui!</Link>
       </main>
     </>
 
