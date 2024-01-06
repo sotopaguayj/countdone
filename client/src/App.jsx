e41e06d10ce8dc4f1a589366e58a48b2652919fe
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-06 09:44:10 -0500

    Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Invitation from './pages/Invitation'
 import Confirm from './pages/Confirm'
+import Footer from './components/Footer'
 
 
-function App () {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={ <Home /> }/>
-        <Route path='invitation/' element={ <Invitation />} />
-        <Route path='confirm_assist/' element={ <Confirm />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='invitation/' element={<Invitation />} />
+            <Route path='confirm_assist/' element={<Confirm />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+      <Footer />
+    </>
   )
 }
 
