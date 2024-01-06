21cd99d9692fdd61759ab44f12ab92eae65691c2
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-05 20:27:36 -0500

    Refactor: Digit Component

--- client/src/components/Digit.jsx
+++ client/src/components/Digit.jsx
@@ -1,16 +1,10 @@
 
-function Digit({ value }) {
-
-  value = value < 10 ? `0${value}` : value.toString()
-  value.split(' ')
-  const [ten, unit] = value
+function Digit({ name, value }) {
 
   return (
     <div>
-      {
-      value <= 10 ? (<span>0</span>) : (<span>{ ten }</span>)
-      }
-      <span>{ unit }</span>
+      <p className='text-sm font-bold uppercase text-slate-50 my-2'>{ name }</p>
+      <div className="bg-[#090909] text-white text-[100px] w-40 rounded-lg mb-12">{ value < 10 ? `0${value}` : value }</div>
     </div>
   )
 }
