f7a018205c96ebba317eff8849a98883f2bdd8d1
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-07 11:54:55 -0500

    Single type alert

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,14 +1,6 @@
 import { motion } from "framer-motion";
 
-function Alert({ title, message, type }) {
-  const bg = {
-    success: "-green-500",
-    error: "-red-500",
-    warning: "-yellow-500",
-  };
-
-  const bgColor = "bg" + bg[type] + "/50".toString();
-  const borderColor = "border" + bg[type].toString();
+function Alert({ title, message }) {
 
   return (
     <motion.div
@@ -18,10 +10,10 @@ function Alert({ title, message, type }) {
       animate={{
         y: 0,
       }}
-      className={`${bgColor} ${borderColor} text-white px-5 py-2 border-l-4 border- flex flex-col items-start`}
+      className={`bg-red-500/50 border-red-500 text-white px-5 py-2 border-l-4 border- flex flex-col items-start`}
     >
-      <h2 className="font-semibold">{title}</h2>
-      <p className="font-light">{message}</p>
+      <h2 className="font-semibold">{ title }</h2>
+      <p className="font-light">{ message }</p>
     </motion.div>
   );
 }
