912b3bc33cec958e86e7893729abeaccfacfc0af
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-10 17:29:25 -0500

    style: :art: Change default export to normal export

--- client/src/api/event.js
+++ client/src/api/event.js
@@ -1,6 +1,5 @@
 
-
-const eventDate = async() => {
+export const eventDate = async() => {
   try {
     const response = await fetch('http://127.0.0.1:8000/event/api/v1/event/af73d455-e036-4322-931c-42463398e3d1')
     const data = await response.json()
@@ -13,5 +12,3 @@ const eventDate = async() => {
     
   }
 }
-
-export default eventDate
