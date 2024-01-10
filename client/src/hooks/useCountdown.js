45cbad95f39c01b6a0ba1488a807f04c94a50618
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-10 17:43:08 -0500

    refactor: :art: Separate counter logic

--- client/src/hooks/useCountdown.js
+++ client/src/hooks/useCountdown.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useDate } from './useDate'
+import { dateCounter } from '../utils/counter'
 
 export const useCountdown = () => {
   const [timeLeft, setTimeLeft] = useState({
@@ -13,30 +14,20 @@ export const useCountdown = () => {
 
   useEffect(() => {
 
-    const dateCounter = () => {
-      const eventDate = new Date(`${ date }T00:00:00`).getTime()
-      const now = new Date().getTime()
-
-      const diff = eventDate - now
-      
-      const minutes = Math.ceil(diff / (1000 * 60))
-      const hours = Math.floor(minutes / 60)
-      const days = Math.floor(hours / 24)
-      const months = Math.floor(days / 30)
-      const minutesLeft = hours >= 60 ?  minutes - (hours * 60) : minutes 
-      const hoursLeft = days >= 1 ?  hours - (days * 24) : hours 
-      const daysLeft = months >= 1 ? days - (months * 30) : days
-      setTimeLeft({
-        minutes: minutesLeft,
-        hours: hoursLeft,
-        days: daysLeft, 
-        months
-      })
-    }
+    const { minutes, hours, days, months } = dateCounter({ date })
+    
+    setTimeLeft({
+      minutes,
+      hours,
+      days, 
+      months
+    })
+    
     const interval = setInterval(dateCounter, 1000)
 
     return () => clearInterval(interval)
-  })
+
+  }, [date])
 
   return { timeLeft, isError, isLoading }
 }
---
+++ client/src/utils/counter.js
@@ -0,0 +1,22 @@
+export const dateCounter = ({ date }) => {
+  
+  const eventDate = new Date(`${ date }T00:00:00`).getTime()
+  const now = new Date().getTime()
+
+  const diff = eventDate - now
+  
+  const minutes = Math.ceil(diff / (1000 * 60))
+  const hours = Math.floor(minutes / 60)
+  const days = Math.floor(hours / 24)
+  const months = Math.floor(days / 30)
+  const minutesLeft = hours >= 24 ?  minutes - (hours * 60) : minutes 
+  const hoursLeft = days >= 1 ?  hours - (days * 24) : hours 
+  const daysLeft = months >= 1 ? days - (months * 30) : days
+  
+  return {
+    minutes: minutesLeft,
+    hours: hoursLeft,
+    days: daysLeft, 
+    months
+  }
+}
