4da6d3869e30c0fcb37423805647aec7a504c6a6
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-11 08:30:14 -0500

    test: :white_check_mark: Perform test suites for dateCounter

--- client/src/utils/counter.js
+++ client/src/utils/counter.js
@@ -1,7 +1,7 @@
-export const dateCounter = ({ date }) => {
+export const dateCounter = ({ date, now }) => {
+  
   
   const eventDate = new Date(`${ date }T00:00:00`).getTime()
-  const now = new Date().getTime()
 
   const diff = eventDate - now
   
@@ -9,6 +9,7 @@ export const dateCounter = ({ date }) => {
   const hours = Math.floor(minutes / 60)
   const days = Math.floor(hours / 24)
   const months = Math.floor(days / 30)
+  // const secondsLeft = minutes >= 60 ?  seconds - (minutes * 60) : seconds 
   const minutesLeft = hours >= 24 ?  minutes - (hours * 60) : minutes 
   const hoursLeft = days >= 1 ?  hours - (days * 24) : hours 
   const daysLeft = months >= 1 ? days - (months * 30) : days
