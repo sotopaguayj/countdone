6d8dcda440fecd7afc4a4c4fcf796e34251410e4
Jose Fajardo <jmfajardorst@gmail.com>
2024-01-14 23:19:41 -0500

    style: :art: Style button component

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,7 +1,7 @@
 
 function Button({ text, type }) {
   return (
-    <button type={ type } className="bg-teal-600 my-2 cursor-pointer text-center hover:bg-teal-500 text-teal-950 transition-colors font-bold py-2 px-4 rounded-sm">
+    <button type={ type } className="bg-teal-600 my-2 cursor-pointer text-center hover:bg-teal-500 text-teal-950 transition-colors font-bold py-2 px-4 rounded-lg text-2xl">
       { text }
     </button>
   );
